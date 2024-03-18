5a659137c6ad19c6044ea8bf568d2d126207a803
Onas-2000 <onasanyadaniel794@gmail.com>
2024-03-18 18:23:12 +0100

    switchPersonHandler was updated to deletePersonHandler

--- src/App.js
+++ src/App.js
@@ -14,17 +14,6 @@ class App extends Component {
     };
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ],
-      otherState: 'some other value',
-      showPersons: false
-    });
-  };
 
  
  togglePersonsHandler = () => {
@@ -32,6 +21,12 @@ class App extends Component {
    this.setState({showPersons: !doesShow});
  }
  
+
+ deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+ }
   render() {
    const style = {
     backgroundColor: 'white',
@@ -46,8 +41,9 @@ class App extends Component {
   if(this.state.showPersons){
     persons = (
       <div>
-      {this.state.persons.map(person => {
+      {this.state.persons.map((person, index )=> {
         return <Person 
+        click= {() => this.deletePersonHandler(index)}
         name= {person.name} 
         age= {person.age}
 
@@ -72,7 +68,7 @@ class App extends Component {
 
       </div>
     );
-  }
+  }n
 }
   
 
