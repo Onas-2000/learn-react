7e4aa8346eef8c2350bb2ec90ed08a381f023c5b
Onas-2000 <onasanyadaniel794@gmail.com>
2024-03-18 21:17:44 +0100

    nameChangedHandler added to create a flexible list

--- src/App.js
+++ src/App.js
@@ -20,7 +20,21 @@ class App extends Component {
    const doesShow = this.state.showPersons;
    this.setState({showPersons: !doesShow});
  }
- 
+ nameChangedHandler = (event, id) =>{
+  const personIndex = this.state.persons.findIndex(p => {
+    return p.id === id;
+  });
+
+  const person ={
+     ...this.state.persons[personIndex]
+  };
+
+  person.name = event.target.value;
+  const persons = [...this.state.persons];
+  persons[personIndex] = person;
+  this.setState({persons: persons});
+ }
+
 
  deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
@@ -48,6 +62,7 @@ class App extends Component {
         name= {person.name} 
         age= {person.age}
         key={person.id}
+        changed={(event) => this.nameChangedHandler(event, person.id)}
         />
       })}
       </div> 
